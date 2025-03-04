961e7b8097b75f88ddf0fb2ba41bf3ad97ce2e9f
QXZ499B <SilviuCostin.Miron@partner.bmw.de>
2025-03-04 11:24:31 +0200

    Cross origin rule

--- src/main/java/org/example/currencyserver/controller/ConversionController.java
+++ src/main/java/org/example/currencyserver/controller/ConversionController.java
@@ -7,12 +7,14 @@ import org.example.currencyserver.service.ConversionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/conversion/v1/")
 public class ConversionController {
